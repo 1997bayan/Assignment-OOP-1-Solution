f61ea15ed9e01c170946ed6d0bb5e661f991d839
Bayan <Bayan@DESKTOP-COKKPVL>
2024-07-08 11:56:27 +0300

    loop through the values of an enum

--- Assignment-opp-1/Program.cs
+++ Assignment-opp-1/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 : Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+            
+            #endregion
         }
     }
 }
